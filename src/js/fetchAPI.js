399da914b9e0b467857b7993b7d3dd1e043efe5b
Tkachenko_Yuriy <uvt86@ukr.net>
2022-11-18 19:44:45 +0200

    update node_modules

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,27 +1,27 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31279729-f68fda383816d4b8eaf8d923a';
 
 
-// export async function getPhotos(request, page) {
-//   try {
-//     const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-  
-
-export function getPhotos(request, page) {
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
-    .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-      return response.json();
-  })
-  
+export async function getPhotos(request, page) {
+  try {
+    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+  
+
+// export function getPhotos(request, page) {
+//     return fetch(`${BASE_URL}?key=${API_KEY}&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+//     .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//       return response.json();
+//   })
+  
+// }
